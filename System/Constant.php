f6d1794c100d2edad4e10846e42a047a41939d97
silassenaprogramador <silas.java.web@gmail.com>
2020-02-15 23:40:16 -0300

    Reestruturando

--- System/Constant.php
+++ System/Constant.php
@@ -6,17 +6,27 @@
 const NAME_SYSTEM = 'Simple Code';
 
 /**
- * ================================
- * Caminhos e diretorios padrões
- * ================================
+ * ==========================================================
+ * nao colocar a ultima barra no final do dominio do projeto
+ * ==========================================================
  */
 const BASE_URL = 'http://localhost/simple-code'; /* http://www.seuprojeto.com.br */
 
 
 /**
- *============================
+ * ==========================================================
+ * banco de dados
+ * ==========================================================
+ */
+const DB_NAME = '';
+const DB_HOST = '';
+const DB_USERNAME = '';
+const DB_PASSWORD = '';
+
+/**
+ *===========================================================
   * Configuraçoes de email
-  *============================
+  *==========================================================
   */
 const EMAIL_SMTP = "";
 const EMAIL_PORTA = 0;
