4c16c7b3851c27f290b1f9172e2c78e0aa071983
silassenaprogramador <silas.java.web@gmail.com>
2020-02-26 12:57:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if(!session_id()) session_start();
 
 require "vendor/autoload.php";
 require_once "System/Constant.php";
-require_once "System/Eloquent.php";
+require_once "System/Database.php";
 
 $routes = require_once __DIR__ ."/routes.php";
 $route = new Ssp\System\Route($routes);
