1ba56a9b1f04b8cc790940d5782e79be833cc5f1
silassenaprogramador <silas.java.web@gmail.com>
2020-02-26 12:59:45 -0300

    Update Controller.php

--- System/Controller.php
+++ System/Controller.php
@@ -16,11 +16,19 @@ class Controller
 
     protected function renderView($viewPath, $dados = [] ){
 
+        //converte o objeto em array
+        if(is_object($dados)){
+            $dados = json_decode($dados,true);     
+        }
+
+        //converte as chaves do array em variaveis 
+        //para usar na view
         foreach($dados as $key => $value){
             $this->params->$key = $value;
         } 
         
         $this->viewPath = $viewPath;
+        //abre a view dentro do controle
         $this->content();
     }
 
@@ -37,7 +45,7 @@ class Controller
         return BASE_URL . $link;
     }
 
-    /**
+    /** 
      * 
      */
     protected function getRequest(){
@@ -47,6 +55,15 @@ class Controller
         $this->objRequest->file = $_FILES;
         
         return $this->objRequest;
+    }
+
+     /** 
+     * 
+     */
+    protected function requestPost(){
+        
+        $this->objRequest->post = $_POST;
+        return $this->objRequest->post;
     }
 
     public function getUri($index){
